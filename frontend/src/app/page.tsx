c9b22097eedde659e50366c659695ff4daaf9585
Deependu Jha <deependujha21@gmail.com>
2024-03-17 20:31:21 +0530

    background of frontend done

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image_Uploader from '@/components/Image_Uploader';
+import { BackgroundGradientAnimation } from '@/components/ui/background-gradient-animation';
+
+const Page = () => {
+	return (
+		<BackgroundGradientAnimation>
+			<Image_Uploader />
+		</BackgroundGradientAnimation>
+	);
+};
+
+export default Page;
---
+++ frontend/src/components/Image_Uploader.tsx
@@ -0,0 +1,8 @@
+'use client';
+import React from 'react';
+
+const Image_Uploader = () => {
+	return <div>Image_Uploader</div>;
+};
+
+export default Image_Uploader;
